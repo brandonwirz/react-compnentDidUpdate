95e1c024fe86b487250a7c8c771d64d92e6a9646
Brandon Wirz <bwirz75@gmail.com>
2019-11-06 13:44:58 -0700

    React lifestyle methods componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// https://scrimba.com/g/greacthooks
+// Uncaught Invariant Violation: Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent infinite loops.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import randomcolor from "randomcolor"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0,
+            color: ""
+        }
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+    }
+
+    increment() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1,
+                color: randomcolor()
+            }
+        })
+    }
+
+decrement() {
+    this.setState(prevState => {
+        return {
+            count: prevState.count - 1,
+            color: randomcolor()
+        }
+    })
 }
 
-export default App;
+    //
+    // componentDidUpdate(prevProps, prevState) {
+    //     if(prevState.count !== this.state.count) {
+    //         const newColor = randomcolor()
+    //         this.setState({color: newColor})
+    //     }
+    // }
+
+    render() {
+        return (
+            <div>
+                <h1 style={{color: this.state.color}}>{this.state.count}</h1>
+                <button onClick={this.increment}>
+                    Increment!
+                </button>
+                <button onClick={this.decrement}>
+                    Decrement!
+                </button>
+            </div>
+        )
+    }
+}
+
+export default App
